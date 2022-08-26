9a62d03da7deac95078e365081a1c370e33f71b6
PabloMarcial99 <17031116@itcelaya.edu.mx>
2022-08-26 11:24:34 -0500

    Commit para deploy clima_app react

---
+++ src/components/AppClima.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Formulario from "./Formulario";
+import Resultado from "./Resultado";
+import Loading from "./Loading";
+import useClima from "../hooks/useClima";
+const AppClima = () => {
+  const { resultado, cargando, noResultado } = useClima();
+  return (
+    <>
+      <main className="dos-columnas">
+        <Formulario></Formulario>
+        {cargando ? (
+          <Loading></Loading>
+        ) : resultado?.name ? (
+          <Resultado></Resultado>
+        ) : noResultado ? (
+          <p>{noResultado}</p>
+        ) : (
+          <p>El clima se va a mostrar aqui</p>
+        )}
+      </main>
+    </>
+  );
+};
+
+export default AppClima;
